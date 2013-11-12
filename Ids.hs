0791bb2150c9c8f27cc9ab9a85b7f6bf9f2db1f5
Jason Stolaruk <jason@detroitlabs.com>
2013-11-12 14:22:36 -0500

    First commit!

---
+++ Ids.hs
@@ -0,0 +1,29 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+module Mud.Ids where
+
+deadEnd = -1 :: Int
+
+iPla = 0 :: Int
+
+-----
+
+iHill = 1 :: Int
+
+iCliff = 2 :: Int
+
+iKewpie1 = 3 :: Int
+
+iKewpie2 = 4 :: Int
+
+iGP1 = 5 :: Int
+
+iGP2 = 6 :: Int
+
+iGP3 = 7 :: Int
+
+iElephant = 8 :: Int
+
+iBag1 = 9 :: Int
+
+iBag2 = 10 :: Int
