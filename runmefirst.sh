c82a77137a15b0942765648a5ca53a0f9b30f155
Jeff Burt <jeffaburt@users.noreply.github.com>
2013-11-12 16:09:11 -0500

    Fixed some weird tabbing issue that got embedded in a script command

--- runmefirst.sh
+++ runmefirst.sh
@@ -8,4 +8,4 @@ cabal install lens
 cabal install text
 
 brew install readline
-cabal install readline --extra-include-dirs=/usr/local/Cellar/readline/6.2.4/include/ --extra-lib-dirs=/usr/local/Cellar/readline/6.2.4/lib/ --configure-option=--with-readline-includes=/usr/local/Cellar/readline/6.2.4/inc‌​lude/ --configure-option=--with-readline-libraries=/usr/local/Cellar/readline/6.2.4/li‌​b/
+cabal install readline --extra-include-dirs=/usr/local/Cellar/readline/6.2.4/include/ --extra-lib-dirs=/usr/local/Cellar/readline/6.2.4/lib/ --configure-option=--with-readline-includes=/usr/local/Cellar/readline/6.2.4/include/ --configure-option=--with-readline-libraries=/usr/local/Cellar/readline/6.2.4/lib/
