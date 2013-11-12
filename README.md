ae8061a1150e953c96a6d63d36c2f8fea2c21591
Jason Stolaruk <jason@detroitlabs.com>
2013-11-12 15:07:01 -0500

    Fixing install/run instructions and script.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ hmud
 ====
 
 A MUD in Haskell.
+
+To play the game:
+
+1. Download the Haskell Platform.
+
+2. Run "runmefirst.sh".
+
+3. From the parent directory of the "Mud" directory, type "runghc Mud/Main.hs".
---
+++ runmefirst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# After installing the Haskell Platform, run this script to grab the packages that the MUD needs.
+
+cabal update
+cabal install cabal-install
+cabal install lens
+cabal install text
+
+brew install readline
+cabal install readline --extra-include-dirs=/usr/local/Cellar/readline/6.2.4/include/ --extra-lib-dirs=/usr/local/Cellar/readline/6.2.4/lib/ --configure-option=--with-readline-includes=/usr/local/Cellar/readline/6.2.4/inc‌​lude/ --configure-option=--with-readline-libraries=/usr/local/Cellar/readline/6.2.4/li‌​b/
